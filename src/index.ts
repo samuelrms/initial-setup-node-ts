7a60b7063fdfa1b74836b58860c2887f3012fa50
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-05-09 02:44:57 -0300

    chore: add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
 import { Request, Response } from "express";
+import * as dotenv from "dotenv";
 import Start from "@/start";
 
 const app = express();
+dotenv.config();
 
-const port = process.env.PORT ?? 3030;
+const port = process.env.PORT || 3030;
 
 app.get("/", (req: Request, res: Response) => {
   res.send(new Start().start());
